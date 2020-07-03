7bbad9db462e389175095cb3b90d261c66c78172
Michel Davit <michel.davit@gmail.com>
2020-07-03 15:27:10 +0200

    Update dependencies (#2)
    * Update dependencies
    
    * Put latest version 1st

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## Unreleased
 
+Update dependencies
+
 ## 0.1.0 (2020-03-01)
 
 Initial release
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val akka      = "2.5.29"
-    val akkaHttp  = "10.1.11"
-    val avro      = "1.9.2"
+    val akka      = "2.5.31"
+    val akkaHttp  = "10.1.12"
+    val avro      = "1.10.0"
     val avro4s    = "3.0.8"
     val logback   = "1.2.3"
-    val scalaTest = "3.1.1"
+    val scalaTest = "3.2.0"
   }
 
   val akkaHttp = "com.typesafe.akka"   %% "akka-http"       % Versions.akkaHttp
