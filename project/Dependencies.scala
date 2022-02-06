77380ad795cc773e5af3b017641e546cc63e906b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-06 15:59:32 +0100

    Update scalatest to 3.2.11 (#105)
    Co-authored-by: Michel Davit <michel.davit@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val avro      = "1.10.2"
     val avro4s    = "3.0.8"
     val logback   = "1.2.10"
-    val scalaTest = "3.2.9"
+    val scalaTest = "3.2.11"
   }
 
   val akkaHttp = "com.typesafe.akka"  %% "akka-http"        % Versions.akkaHttp
