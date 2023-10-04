62d5c5039f8a97f761deeccbb0ea6c556f8c159b
Michel Davit <michel@davit.fr>
2023-10-04 09:35:47 +0200

    Full avro spec

--- src/main/scala/fr/davit/pekko/http/scaladsl/marshallers/avro/AvroCoder.scala
+++ src/main/scala/fr/davit/pekko/http/scaladsl/marshallers/avro/AvroCoder.scala
@@ -17,64 +17,200 @@
 package fr.davit.pekko.http.scaladsl.marshallers.avro
 
 import org.apache.avro.Schema
-import org.apache.avro.io.{DecoderFactory, EncoderFactory}
-import org.apache.avro.message.{BinaryMessageDecoder, BinaryMessageEncoder}
-import org.apache.avro.reflect.ReflectData
-import org.apache.avro.specific.{SpecificData, SpecificDatumReader, SpecificDatumWriter}
+import org.apache.avro.generic.{GenericData, GenericDatumReader, GenericDatumWriter, GenericRecord}
+import org.apache.avro.io.{DatumReader, DatumWriter, DecoderFactory, EncoderFactory}
+import org.apache.avro.message.{BinaryMessageDecoder, BinaryMessageEncoder, RawMessageDecoder, RawMessageEncoder}
+import org.apache.avro.reflect.{ReflectData, ReflectDatumReader, ReflectDatumWriter}
+import org.apache.avro.specific.{SpecificData, SpecificDatumReader, SpecificDatumWriter, SpecificRecord}
 import org.apache.pekko.util.ByteString
 
-import scala.reflect.ClassTag
+import scala.reflect.{classTag, ClassTag}
 
-abstract class AvroCoder[T: ClassTag] {
-
-  protected lazy val schema: Schema = ReflectData.get().getSchema(implicitly[ClassTag[T]].runtimeClass)
-
-  def encode(model: T): ByteString
-
-  def decode(data: ByteString): T
-
+trait AvroEncoder[T] {
+  def encode(record: T): ByteString
 }
 
-class AvroBinaryCoder[T: ClassTag] extends AvroCoder[T] {
+trait AvroDecoder[T] {
+  def decode(bytes: ByteString): T
+}
 
+///////////////////////////////////////////////////////////////////////////////
+// Binary
+///////////////////////////////////////////////////////////////////////////////
+class AvroBinaryEncoder[T](model: GenericData, schema: Schema) extends AvroEncoder[T] {
   // set shouldCopy to false since ByteString.apply will make the copy
-  private lazy val messageEncoder = new BinaryMessageEncoder[T](new SpecificData(), schema, false)
+  private lazy val messageEncoder = new BinaryMessageEncoder[T](model, schema, false)
 
-  private lazy val messageDecoder = new BinaryMessageDecoder[T](new SpecificData(), schema)
-
-  override def encode(model: T): ByteString = ByteString(messageEncoder.encode(model))
-
-  override def decode(data: ByteString): T = messageDecoder.decode(data.asByteBuffer)
+  override def encode(record: T): ByteString = ByteString(messageEncoder.encode(record))
 }
 
-class AvroJsonCoder[T: ClassTag] extends AvroCoder[T] {
+object AvroBinaryEncoder {
+  def generic(schema: Schema): AvroBinaryEncoder[GenericRecord] = {
+    new AvroBinaryEncoder(GenericData.get(), schema)
+  }
 
-  private lazy val jsonWriter = new SpecificDatumWriter[T](schema)
+  def specific[T <: SpecificRecord: ClassTag]: AvroBinaryEncoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = SpecificData.getForClass(clazz)
+    val schema = model.getSchema(clazz)
+    new AvroBinaryEncoder(model, schema)
+  }
 
-  private lazy val jsonReader = new SpecificDatumReader[T](schema)
+  def reflect[T: ClassTag]: AvroBinaryEncoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = ReflectData.get()
+    val schema = model.getSchema(clazz)
+    new AvroBinaryEncoder(model, schema)
+  }
+}
 
-  override def encode(avro: T): ByteString = {
+class AvroBinaryDecoder[T](model: GenericData, schema: Schema) extends AvroDecoder[T] {
+  private lazy val messageDecoder = new BinaryMessageDecoder[T](model, schema)
+
+  override def decode(bytes: ByteString): T = messageDecoder.decode(bytes.asByteBuffer)
+}
+
+object AvroBinaryDecoder {
+
+  // writer schema will be used
+  def generic: AvroBinaryDecoder[GenericRecord] = generic(null)
+
+  def generic(schema: Schema): AvroBinaryDecoder[GenericRecord] = {
+    new AvroBinaryDecoder(GenericData.get(), schema)
+  }
+
+  def specific[T <: SpecificRecord: ClassTag]: AvroBinaryDecoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = SpecificData.getForClass(clazz)
+    val schema = model.getSchema(clazz)
+    new AvroBinaryDecoder(model, schema)
+  }
+
+  def reflect[T: ClassTag]: AvroBinaryDecoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = ReflectData.get()
+    val schema = model.getSchema(clazz)
+    new AvroBinaryDecoder(model, schema)
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Raw
+/////////////////////////////////////////////////////////////////////////////
+class AvroRawEncoder[T](model: GenericData, schema: Schema) extends AvroEncoder[T] {
+  // set shouldCopy to false since ByteString.apply will make the copy
+  private lazy val messageEncoder = new RawMessageEncoder[T](model, schema, false)
+
+  override def encode(record: T): ByteString = ByteString(messageEncoder.encode(record))
+}
+
+object AvroRawEncoder {
+  def generic(schema: Schema): AvroRawEncoder[GenericRecord] = {
+    new AvroRawEncoder(GenericData.get(), schema)
+  }
+
+  def specific[T <: SpecificRecord: ClassTag]: AvroRawEncoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = SpecificData.getForClass(clazz)
+    val schema = model.getSchema(clazz)
+    new AvroRawEncoder(model, schema)
+  }
+
+  def reflect[T: ClassTag]: AvroRawEncoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = ReflectData.get()
+    val schema = model.getSchema(clazz)
+    new AvroRawEncoder(model, schema)
+  }
+}
+
+class AvroRawDecoder[T](model: GenericData, schema: Schema) extends AvroDecoder[T] {
+  private lazy val messageDecoder = new RawMessageDecoder[T](model, schema)
+
+  override def decode(bytes: ByteString): T = messageDecoder.decode(bytes.asByteBuffer)
+}
+
+object AvroRawDecoder {
+  def generic(schema: Schema): AvroRawDecoder[GenericRecord] = {
+    new AvroRawDecoder(GenericData.get(), schema)
+  }
+
+  def specific[T <: SpecificRecord: ClassTag]: AvroRawDecoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = SpecificData.getForClass(clazz)
+    val schema = model.getSchema(clazz)
+    new AvroRawDecoder(model, schema)
+  }
+
+  def reflect[T: ClassTag]: AvroRawDecoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val model  = ReflectData.get()
+    val schema = model.getSchema(clazz)
+    new AvroRawDecoder(model, schema)
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Json
+///////////////////////////////////////////////////////////////////////////////
+class AvroJsonEncoder[T](writer: DatumWriter[T], schema: Schema) extends AvroEncoder[T] {
+
+  override def encode(model: T): ByteString = {
     val builder = ByteString.newBuilder
     val encoder = EncoderFactory.get.jsonEncoder(schema, builder.asOutputStream)
-    jsonWriter.write(avro, encoder)
+    writer.write(model, encoder)
     encoder.flush()
     builder.result()
   }
+}
 
+object AvroJsonEncoder {
+  def generic(schema: Schema): AvroJsonEncoder[GenericRecord] = {
+    val writer = new GenericDatumWriter[GenericRecord]
+    new AvroJsonEncoder(writer, schema)
+  }
+
+  def specific[T <: SpecificRecord: ClassTag]: AvroJsonEncoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val writer = new SpecificDatumWriter(clazz)
+    val schema = SpecificData.get().getSchema(clazz)
+    new AvroJsonEncoder(writer, schema)
+  }
+
+  def reflect[T: ClassTag]: AvroJsonEncoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val writer = new ReflectDatumWriter(clazz)
+    val schema = ReflectData.get().getSchema(clazz)
+    new AvroJsonEncoder(writer, schema)
+  }
+
+}
+
+class AvroJsonDecoder[T](reader: DatumReader[T], schema: Schema) extends AvroDecoder[T] {
   override def decode(data: ByteString): T = {
     val decoder = DecoderFactory.get.jsonDecoder(schema, data.iterator.asInputStream)
-    jsonReader.read(null.asInstanceOf[T], decoder)
+    reader.read(null.asInstanceOf[T], decoder)
   }
 }
 
-trait AvroCoderFactory {
-  def newCoder[T: ClassTag]: AvroCoder[T]
-}
+object AvroJsonDecoder {
+  def generic(schema: Schema): AvroJsonDecoder[GenericRecord] = {
+    val reader = new GenericDatumReader[GenericRecord]
+    new AvroJsonDecoder(reader, schema)
+  }
 
-object AvroBinaryCoderFactory extends AvroCoderFactory {
-  override def newCoder[T: ClassTag]: AvroCoder[T] = new AvroBinaryCoder[T]()
-}
+  def specific[T <: SpecificRecord: ClassTag]: AvroJsonDecoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val reader = new SpecificDatumReader(clazz)
+    val schema = SpecificData.get().getSchema(clazz)
+    new AvroJsonDecoder(reader, schema)
+  }
 
-object AvroJsonCoderFactory extends AvroCoderFactory {
-  override def newCoder[T: ClassTag]: AvroCoder[T] = new AvroJsonCoder[T]()
+  def reflect[T: ClassTag]: AvroJsonDecoder[T] = {
+    val clazz  = classTag[T].runtimeClass.asInstanceOf[Class[T]]
+    val reader = new ReflectDatumReader(clazz)
+    val schema = ReflectData.get().getSchema(clazz)
+    new AvroJsonDecoder(reader, schema)
+  }
+
 }
--- src/main/scala/fr/davit/pekko/http/scaladsl/marshallers/avro/AvroSupport.scala
+++ src/main/scala/fr/davit/pekko/http/scaladsl/marshallers/avro/AvroSupport.scala
@@ -16,74 +16,189 @@
 
 package fr.davit.pekko.http.scaladsl.marshallers.avro
 
-import org.apache.avro.specific.SpecificRecordBase
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericRecord
+import org.apache.avro.specific.SpecificRecord
 import org.apache.pekko.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
+import org.apache.pekko.http.scaladsl.model.*
 import org.apache.pekko.http.scaladsl.model.MediaType.NotCompressible
-import org.apache.pekko.http.scaladsl.model._
 import org.apache.pekko.http.scaladsl.unmarshalling.{FromEntityUnmarshaller, Unmarshaller}
 
 import scala.reflect.ClassTag
 
 object AvroProtocol {
   val `avro/binary`: ContentType = MediaType.customBinary("avro", "binary", NotCompressible)
+  val `avro/raw`: ContentType    = MediaType.customBinary("avro", "raw", NotCompressible)
 }
 
-trait AvroAbstractSupport {
+trait AvroGenericSupport {
+  def avroGenericUnmarshaller(schema: Schema): FromEntityUnmarshaller[GenericRecord]
+  def avroGenericMarshaller(schema: Schema): ToEntityMarshaller[GenericRecord]
+}
 
-  def contentTypes: Seq[ContentType]
+trait AvroSpecificSupport {
+  implicit def avroSpecificUnmarshaller[T <: SpecificRecord: ClassTag]: FromEntityUnmarshaller[T]
+  implicit def avroSpecificMarshaller[T <: SpecificRecord: ClassTag]: ToEntityMarshaller[T]
+}
 
-  protected def coderFactory: AvroCoderFactory
+trait AvroReflectSupport {
+  def avroReflectUnmarshaller[T: ClassTag]: FromEntityUnmarshaller[T]
+  def avroReflectMarshaller[T: ClassTag]: ToEntityMarshaller[T]
+}
 
-  // --------------------------------------------------------------------------------------------------------------------
-  // Unmarshallers
-  // --------------------------------------------------------------------------------------------------------------------
-  implicit def avroUnmarshaller[T <: SpecificRecordBase: ClassTag]: FromEntityUnmarshaller[T] = {
-    val coder = coderFactory.newCoder[T]
+trait AvroAllSupport             extends AvroGenericSupport with AvroSpecificSupport with AvroReflectSupport
+trait AvroProtocolGenericSupport extends AvroGenericSupport {
+
+  def contentType: ContentType
+
+  def genericDecoder(schema: Schema): AvroDecoder[GenericRecord]
+
+  def genericEncoder(schema: Schema): AvroEncoder[GenericRecord]
+
+  override def avroGenericUnmarshaller(schema: Schema): FromEntityUnmarshaller[GenericRecord] = {
+    val decoder = genericDecoder(schema)
     Unmarshaller.byteStringUnmarshaller
-      .forContentTypes(contentTypes.map(ContentTypeRange.apply): _*)
-      .map(coder.decode)
+      .forContentTypes(contentType)
+      .map(decoder.decode)
   }
 
-  // --------------------------------------------------------------------------------------------------------------------
-  // Marshallers
-  // --------------------------------------------------------------------------------------------------------------------
-  implicit def avroMarshaller[T <: SpecificRecordBase: ClassTag]: ToEntityMarshaller[T] = {
-    val coder = coderFactory.newCoder[T]
-    Marshaller.oneOf(contentTypes.map(ct => Marshaller.ByteStringMarshaller.wrap(ct.mediaType)(coder.encode)): _*)
+  override def avroGenericMarshaller(schema: Schema): ToEntityMarshaller[GenericRecord] = {
+    val encoder = genericEncoder(schema)
+    Marshaller
+      .byteStringMarshaller(contentType)
+      .compose(encoder.encode)
   }
 }
 
-trait AvroBinarySupport extends AvroAbstractSupport {
-  override lazy val contentTypes: Seq[ContentType]           = Seq(AvroProtocol.`avro/binary`)
-  override protected lazy val coderFactory: AvroCoderFactory = AvroBinaryCoderFactory
+trait AvroProtocolSpecificSupport extends AvroSpecificSupport {
+
+  def contentType: ContentType
+
+  def specificDecoder[T <: SpecificRecord: ClassTag]: AvroDecoder[T]
+
+  def specificEncoder[T <: SpecificRecord: ClassTag]: AvroEncoder[T]
+
+  implicit override def avroSpecificUnmarshaller[T <: SpecificRecord: ClassTag]: FromEntityUnmarshaller[T] = {
+    val decoder = specificDecoder[T]
+    Unmarshaller.byteStringUnmarshaller
+      .forContentTypes(contentType)
+      .map(decoder.decode)
+  }
+
+  implicit override def avroSpecificMarshaller[T <: SpecificRecord: ClassTag]: ToEntityMarshaller[T] = {
+    val encoder = specificEncoder[T]
+    Marshaller
+      .byteStringMarshaller(contentType)
+      .compose(encoder.encode)
+  }
+}
+
+trait AvroProtocolReflectSupport extends AvroReflectSupport {
+
+  def contentType: ContentType
+
+  def reflectDecoder[T: ClassTag]: AvroDecoder[T]
+
+  def reflectEncoder[T: ClassTag]: AvroEncoder[T]
+  override def avroReflectUnmarshaller[T: ClassTag]: FromEntityUnmarshaller[T] = {
+    val decoder = reflectDecoder[T]
+    Unmarshaller.byteStringUnmarshaller
+      .forContentTypes(contentType)
+      .map(decoder.decode)
+  }
+
+  override def avroReflectMarshaller[T: ClassTag]: ToEntityMarshaller[T] = {
+    val encoder = reflectEncoder[T]
+    Marshaller
+      .byteStringMarshaller(contentType)
+      .compose(encoder.encode)
+  }
+}
+
+trait AvroProtocolAllSupport
+    extends AvroAllSupport
+    with AvroProtocolGenericSupport
+    with AvroProtocolSpecificSupport
+    with AvroProtocolReflectSupport
+
+trait AvroBinarySupport extends AvroProtocolAllSupport {
+  override def contentType: ContentType                                       = AvroProtocol.`avro/binary`
+  override def genericDecoder(schema: Schema): AvroDecoder[GenericRecord]     = AvroBinaryDecoder.generic(schema)
+  override def genericEncoder(schema: Schema): AvroEncoder[GenericRecord]     = AvroBinaryEncoder.generic(schema)
+  override def specificDecoder[T <: SpecificRecord: ClassTag]: AvroDecoder[T] = AvroBinaryDecoder.specific[T]
+  override def specificEncoder[T <: SpecificRecord: ClassTag]: AvroEncoder[T] = AvroBinaryEncoder.specific[T]
+  override def reflectDecoder[T: ClassTag]: AvroDecoder[T]                    = AvroBinaryDecoder.reflect[T]
+  override def reflectEncoder[T: ClassTag]: AvroEncoder[T]                    = AvroBinaryEncoder.reflect[T]
 }
 
 object AvroBinarySupport extends AvroBinarySupport
 
-trait AvroJsonSupport extends AvroAbstractSupport {
-  override lazy val contentTypes: Seq[ContentType]           = Seq(ContentTypes.`application/json`)
-  override protected lazy val coderFactory: AvroCoderFactory = AvroJsonCoderFactory
+trait AvroRawSupport extends AvroProtocolAllSupport {
+  override def contentType: ContentType                                       = AvroProtocol.`avro/raw`
+  override def genericDecoder(schema: Schema): AvroDecoder[GenericRecord]     = AvroRawDecoder.generic(schema)
+  override def genericEncoder(schema: Schema): AvroEncoder[GenericRecord]     = AvroRawEncoder.generic(schema)
+  override def specificDecoder[T <: SpecificRecord: ClassTag]: AvroDecoder[T] = AvroRawDecoder.specific[T]
+  override def specificEncoder[T <: SpecificRecord: ClassTag]: AvroEncoder[T] = AvroRawEncoder.specific[T]
+  override def reflectDecoder[T: ClassTag]: AvroDecoder[T]                    = AvroRawDecoder.reflect[T]
+  override def reflectEncoder[T: ClassTag]: AvroEncoder[T]                    = AvroRawEncoder.reflect[T]
+}
+
+object AvroRawSupport extends AvroRawSupport
+
+trait AvroJsonSupport extends AvroProtocolAllSupport {
+  override def contentType: ContentType                                       = ContentTypes.`application/json`
+  override def genericDecoder(schema: Schema): AvroDecoder[GenericRecord]     = AvroJsonDecoder.generic(schema)
+  override def genericEncoder(schema: Schema): AvroEncoder[GenericRecord]     = AvroJsonEncoder.generic(schema)
+  override def specificDecoder[T <: SpecificRecord: ClassTag]: AvroDecoder[T] = AvroJsonDecoder.specific[T]
+  override def specificEncoder[T <: SpecificRecord: ClassTag]: AvroEncoder[T] = AvroJsonEncoder.specific[T]
+  override def reflectDecoder[T: ClassTag]: AvroDecoder[T]                    = AvroJsonDecoder.reflect[T]
+  override def reflectEncoder[T: ClassTag]: AvroEncoder[T]                    = AvroJsonEncoder.reflect[T]
 }
 
 object AvroJsonSupport extends AvroJsonSupport
 
-trait AvroSupport extends AvroAbstractSupport {
+trait AvroSupport extends AvroAllSupport {
+  override def avroGenericUnmarshaller(schema: Schema): FromEntityUnmarshaller[GenericRecord] =
+    Unmarshaller.firstOf(
+      AvroJsonSupport.avroGenericUnmarshaller(schema),
+      AvroRawSupport.avroGenericUnmarshaller(schema),
+      AvroBinarySupport.avroGenericUnmarshaller(schema)
+    )
 
-  override protected def coderFactory: AvroCoderFactory =
-    throw new Exception("No avro coder factory defined for AvroSupport")
+  override def avroGenericMarshaller(schema: Schema): ToEntityMarshaller[GenericRecord] =
+    Marshaller.oneOf(
+      AvroJsonSupport.avroGenericMarshaller(schema),
+      AvroRawSupport.avroGenericMarshaller(schema),
+      AvroBinarySupport.avroGenericMarshaller(schema)
+    )
 
-  private val avroSupports = Seq(AvroJsonSupport, AvroBinarySupport)
+  override implicit def avroSpecificUnmarshaller[T <: SpecificRecord: ClassTag]: FromEntityUnmarshaller[T] =
+    Unmarshaller.firstOf(
+      AvroJsonSupport.avroSpecificUnmarshaller[T],
+      AvroRawSupport.avroSpecificUnmarshaller[T],
+      AvroBinarySupport.avroSpecificUnmarshaller[T]
+    )
 
-  override val contentTypes: Seq[ContentType] = avroSupports.flatMap(_.contentTypes)
+  override implicit def avroSpecificMarshaller[T <: SpecificRecord: ClassTag]: ToEntityMarshaller[T] =
+    Marshaller.oneOf(
+      AvroJsonSupport.avroSpecificMarshaller[T],
+      AvroRawSupport.avroSpecificMarshaller[T],
+      AvroBinarySupport.avroSpecificMarshaller[T]
+    )
 
-  implicit override def avroUnmarshaller[T <: SpecificRecordBase: ClassTag]: FromEntityUnmarshaller[T] = {
-    Unmarshaller.firstOf(avroSupports.map(_.avroUnmarshaller[T]): _*)
-  }
+  override def avroReflectUnmarshaller[T: ClassTag]: FromEntityUnmarshaller[T] =
+    Unmarshaller.firstOf(
+      AvroJsonSupport.avroReflectUnmarshaller[T],
+      AvroRawSupport.avroReflectUnmarshaller[T],
+      AvroBinarySupport.avroReflectUnmarshaller[T]
+    )
 
-  implicit override def avroMarshaller[T <: SpecificRecordBase: ClassTag]: ToEntityMarshaller[T] = {
-    Marshaller.oneOf(avroSupports.map(_.avroMarshaller[T]): _*)
-  }
-
+  override def avroReflectMarshaller[T: ClassTag]: ToEntityMarshaller[T] =
+    Marshaller.oneOf(
+      AvroJsonSupport.avroReflectMarshaller[T],
+      AvroRawSupport.avroReflectMarshaller[T],
+      AvroBinarySupport.avroReflectMarshaller[T]
+    )
 }
 
 object AvroSupport extends AvroSupport
