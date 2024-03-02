b4e72001024162f0732852248066b295583de954
Scala Steward <scala_steward@virtuslab.com>
2024-03-02 23:11:56 +0000

    Update logback-classic to 1.5.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val avro      = "1.11.3"
-    val logback   = "1.4.14"
+    val logback   = "1.5.2"
     val pekko     = "1.0.2"
     val pekkoHttp = "1.0.0"
     val scalaTest = "3.2.17"
