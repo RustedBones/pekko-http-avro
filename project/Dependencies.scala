fb2939219adcde591708d5f46f11e8a5621fb35f
Michel Davit <michel@davit.fr>
2024-04-09 23:09:14 +0200

    Move logback to test scope

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,11 +14,11 @@ object Dependencies {
   val pekkoHttp = "org.apache.pekko" %% "pekko-http" % Versions.pekkoHttp
 
   object Provided {
-    val logback     = "ch.qos.logback"    % "logback-classic" % Versions.logback % "provided"
-    val pekkoStream = "org.apache.pekko" %% "pekko-stream"    % Versions.pekko   % "provided"
+    val pekkoStream = "org.apache.pekko" %% "pekko-stream" % Versions.pekko % "provided"
   }
 
   object Test {
+    val logback          = "ch.qos.logback"    % "logback-classic"    % Versions.logback   % "test"
     val pekkoHttpTestkit = "org.apache.pekko" %% "pekko-http-testkit" % Versions.pekkoHttp % "test"
     val pekkoTestkit     = "org.apache.pekko" %% "pekko-testkit"      % Versions.pekko     % "test"
     val scalaTest        = "org.scalatest"    %% "scalatest"          % Versions.scalaTest % "test"
